0f3c02441c334870b6ee31eae45006f817982ba6
amine <mesbahminos@gmail.com>
2024-06-26 14:54:52 +0100

    add human choice

--- script.js
+++ script.js
@@ -3,5 +3,20 @@ function getComputerChoice(array) {
   return array[randomIndex];
 }
 
+function getHumanChoice() {
+  let userInput = prompt("enter ur choice");
+  if (userInput.toLowerCase() === choices[0]) {
+    return "rock";
+  } else if (userInput.toLowerCase() === choices[1]) {
+    return "paper";
+  } else if (userInput.toLowerCase() === choices[2]) {
+    return "scissors";
+  } else {
+    alert("enter a valid choice !");
+    return getHumanChoice();
+  }
+}
+
 let choices = ["rock", "paper", "scissors"];
 let computerChoice = getComputerChoice(choices);
+let humanChoice = getHumanChoice();
